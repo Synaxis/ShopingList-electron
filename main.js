794be4ef0d39cfa93d99225865efd3d413d89335
Synaxis <01xtor@gmail.com>
2018-02-15 19:31:39 -0200

    accelerators

---
+++ main.js
@@ -0,0 +1,47 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+
+// Listen for app to be ready
+app.on('ready', function(){
+    // Create new window
+    mainWindow = new BrowserWindow({});
+    // Load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol:'file:',
+        slashe: true
+    }));
+
+    // Build menu From template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    // Insert Menu
+    Menu.setApplicationMenu(mainMenu);
+});
+
+// create menu template
+const mainMenuTemplate = [
+    {
+        label:'File',
+        submenu:[
+            {
+                label: 'Add Item'
+            },
+            {
+                label :'Clear Items'
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q':
+                'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
